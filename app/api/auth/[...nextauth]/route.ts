7941f6f10691c9888f7ecfb665fe94ca6313abd2
RianaGit24 <parthsavla619@gmail.com>
2025-06-28 17:18:28 +0300

    Initial commit

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,9 +0,0 @@
-// app/api/auth/[...nextauth]/route.ts
-
-import { authOptions } from "@/lib/auth";
-import NextAuth from "next-auth";
-
-const handler = NextAuth(authOptions);
-
-// Export GET and POST for Next.js App Router
-export { handler as GET, handler as POST };
